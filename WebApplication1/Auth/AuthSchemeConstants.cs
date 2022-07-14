e0f1be81d18771ec58bf0a43d30976dc677e78a8
aidankhaligova <aydan.abdulkhaliqova@gts.az>
2022-07-14 16:34:37 +0400

    Add project files.

---
+++ WebApplication1/Auth/AuthSchemeConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomAuth.Auth
+{
+    public class AuthSchemeConstants
+    {
+        public const string MyAuthScheme = "Ninpo";
+        public const string NToken = $"{MyAuthScheme} (?<token>.*)";
+    }
+}
---
+++ WebApplication1/Auth/TokenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomAuth.Auth
+{
+    public class TokenModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
